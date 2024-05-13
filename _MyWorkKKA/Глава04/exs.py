20b94fd43790cd00f8096344bbfb030ebcc360e6
Kirill Kuznetsov <herdboy@yandex.ru>
2024-05-13 23:27:27 +0300

    Страница 81

---
+++ _MyWorkKKA/Глава04/exs.py
@@ -0,0 +1,10 @@
+#%%
+first_letter = input("Tell me your password:")
+print(first_letter.upper()[:1])
+
+#%%
+int(2)
+
+#%%
+int("12.0")
+# %%
---
+++ _MyWorkKKA/Глава04/first_letter.py
@@ -0,0 +1,2 @@
+first_letter = input("Tell me your password:")
+print(first_letter.upper()[:1])
